9d11672f1f37a5d907b88d9cf745e408245d241a
Jason X <59701038+JChunX@users.noreply.github.com>
2022-01-28 22:48:09 -0500

    procedural staircases

--- setup.py
+++ setup.py
@@ -1,10 +1,35 @@
+import os
+from glob import glob
+
 from setuptools import setup
 
 package_name = 'obstacles'
 
+data_files = [
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml'])
+    ]
+
+def glob_recursive(data_files, directory):
+    files = glob(directory+'*.*')
+    data_files.append((os.path.join('share', package_name, directory), files))
+    subdirectories = glob(directory+'*/')
+    if (subdirectories == []):
+        return
+    else:
+        for dir in subdirectories:
+            glob_recursive(data_files, dir)
+
+data_directories = ['urdf']
+
+for directory in data_directories:
+    glob_recursive(data_files, directory)
+
+
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -15,8 +40,8 @@ setup(
     zip_safe=True,
     maintainer='jasonx',
     maintainer_email='59701038+JChunX@users.noreply.github.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Modular URDF Obstacles',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
